0b94e8420ce61cb24b51d55cce36605f9821fb84
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-08-05 21:53:48 +0300

    Task8 done

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -2,17 +2,17 @@
 // показывает все чётные числа от 1 до N.
 
 
-Console.WriteLine("Укажите целое число");
-int num = Convert.ToInt32(Concole.ReadLine());
-int i = 2;
-for ( i <= num ; i += 2 )
-        Console.WriteLine(i);
-        // Console.Write(+i);
-
-// Console.WriteLine("Введите длину: ");
-// int n = int.Parse(Console.ReadLine());
-    
-// for (int i = 1; i <= n; i++)
-//     if (i % 2 == 0)
+// Console.WriteLine("Укажите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// for (i <= num; i += 2)
 //         Console.WriteLine(i);
+
+
+Console.WriteLine("Введите целое число  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
 
