b59c3f60663b87410f09865970cb7507792fd9bf
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-08-05 15:38:53 +0300

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+Console.WriteLine("Введите число a ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+  if ( numb1 > numb2 )
+{ 
+Console.WriteLine($" Число {numb1} > {numb2} ");
+}
+else
+{
+    Console.WriteLine($"Число {numb2} > {numb1} ");
+}
+
+
+
