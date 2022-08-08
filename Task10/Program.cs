8791a98fb061133a3de967997dc3b71a3ce7c9c9
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-08-08 13:13:15 +0300

    Task10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3х значное число  ");
+int numbFirst = Convert.ToInt32(Console.ReadLine());
+int Convert(int numb)
+{
+    result((numb / 10) % 10);
+}
+int result =Convert(numbFirst);
+Console.WriteLine($"Вторая цифра числа {numbFirst} это {result}");
