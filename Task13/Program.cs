0f9279465e92cda1f9e550d4e5b567644215ad85
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-08-11 20:23:15 +0300

    Task15 done

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного  
+// числа или сообщает, что третьей цифры нет. Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите  число");
+int numb = Convert.ToInt32(Console.ReadLine());
+int ThedNumb(int number)
+{
+    while (number > 999) number = number / 10;
+    number = number % 10;
+    return number;
+}
+int result = ThedNumb(numb);
+if (numb < 100)
+{
+    Console.WriteLine("Указано некорректное число");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа {numb} это {result}");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Укажите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+bool Weekend(int day1)
+{
+    return day1 == 6 || day1 == 7  ;
+}
+bool result = Weekend(day);
+Console.WriteLine(result ? "Да , это выходной" : "Нет , это не выходной");
