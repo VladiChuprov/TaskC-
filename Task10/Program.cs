f731b65890a90baf9d8f654998768ec97d099f6d
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-08-11 20:33:15 +0300

    Task

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Введите 3х значное число  ");
 int numbFirst = Convert.ToInt32(Console.ReadLine());
 int SecondNumb(int numb)
 {
-    return (numb / 10) % 10;
+  return (numb / 10) % 10;    
 }
 int result = SecondNumb(numbFirst);
 Console.WriteLine($"Вторая цифра числа {numbFirst} это {result}");
