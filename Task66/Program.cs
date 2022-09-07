6c04ddcf3b3de2ac0a33ce3c73daee2c5dbdec3d
Vladimir Chuprov <xxazxxaz@yandex.ru>
2022-09-07 19:40:34 +0300

    Task68 done

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+Console.Write("Введите число ");
+int number1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void SumNaturalNumber(int num1 , int num2,int sum)
+{
+    sum = sum + (num1++);    
+    if (num1> num2) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    
+    SumNaturalNumber(num1, num2,sum);  
+    }
+
+SumNaturalNumber(number1 , number2,0);
---
+++ Task68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n. 
+
+Console.Write("Введите число m ");
+int number1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int AkkFunction(int num1 , int num2)
+{
+    if (num1 ==0)return num2+1;
+    if (num1 >0 && num2==0)return AkkFunction(num1-1,1);
+    if (num1 >0 && num2>0)return AkkFunction (num1-1 ,AkkFunction(num1,num2-1));
+return AkkFunction(num1, num2);
+}
+
+System.Console.WriteLine($"Функция Аккермана для чисел A({number1},{number2}) = {AkkFunction(number1, number2)}" );
